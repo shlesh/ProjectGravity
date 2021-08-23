121b9799e97973b59f846d77b87e8c535d19f39e
Shailesh Tiwari <iib2020001@iiita.ac.in>
2021-08-23 16:58:27 +0400

    hi

--- src/screens/Members.js
+++ src/screens/Members.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Members(props) {
-  return (
-    <div>
-      <p>heyyy members here</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>heyyy members here</div>
+		</div>
+	);
 }
 
 export default Members;
